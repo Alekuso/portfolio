269af425bec5b7e29d4ea331af927352ee9e111c
Alex <alexandre.olemans@gmail.com>
2022-06-20 03:18:52 +0200

    temp fix to themes

--- src/components/ThemeButton.svelte
+++ src/components/ThemeButton.svelte
@@ -6,7 +6,9 @@
 
 		if ($theme == 'light') {
 			document.body.classList.add('light');
+			document.body.classList.remove('dark');
 		} else {
+			document.body.classList.add('dark');
 			document.body.classList.remove('light');
 		}
 	}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,9 @@
 
 	if ($theme == 'light') {
 		document.body.classList.add('light');
+		document.body.classList.remove('dark');
 	} else {
+		document.body.classList.add('dark');
 		document.body.classList.remove('light');
 	}
 </script>
